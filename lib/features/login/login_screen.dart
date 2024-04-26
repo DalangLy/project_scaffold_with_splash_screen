ebb026030c7442e870c79d312076f24d232c04e7
Dalang <dalangly@gmail.com>
2024-04-26 16:14:36 +0700

    rename text

--- lib/features/login/login_screen.dart
+++ lib/features/login/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get/get_connect/http/src/utils/utils.dart';
 
 import '../../core/services/auth_service.dart';
 
@@ -12,33 +13,37 @@ class LoginScreen extends StatelessWidget {
       appBar: AppBar(),
       body: SafeArea(
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  AuthService.to.login();
-                  final thenTo = context.params['then'];
-                  Get.offNamed(thenTo ?? '/home');
-                },
-                child: const Text("Login"),
-              ),
-              const SizedBox(height: 10,),
-              ElevatedButton(
-                onPressed: (){
-                  AuthService.to.login();
-                },
-                child: const Text("Logout but not redirect to home screen"),
-              ),
-              const SizedBox(height: 10,),
-              ElevatedButton(
-                onPressed: () {
-                  final thenTo = context.params['then'];
-                  Get.offNamed(thenTo ?? '/home');
-                },
-                child: const Text("Goto Home Screen Directly"),
-              ),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 10,),
+                ElevatedButton(
+                  onPressed: () {
+                    AuthService.to.login();
+                    final thenTo = context.params['then'];
+                    Get.offNamed(thenTo ?? '/home');
+                  },
+                  child: const Text("Login"),
+                ),
+                const SizedBox(height: 10,),
+                ElevatedButton(
+                  onPressed: (){
+                    AuthService.to.login();
+                  },
+                  child: const Text("Login but not redirect to home screen"),
+                ),
+                const SizedBox(height: 10,),
+                ElevatedButton(
+                  onPressed: () {
+                    final thenTo = context.params['then'];
+                    Get.offNamed(thenTo ?? '/home');
+                  },
+                  child: const Text("Goto Home Screen Directly"),
+                ),
+              ],
+            ),
           ),
         ),
       ),
